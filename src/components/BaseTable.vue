59fbf396adaccf1348cb562fcd774462d1f0336e
supperT <97022976@QQ.com>
2022-07-08 21:25:29 +0800

    feat(computed basetable): 封装公共table组件实现快速渲染页面

---
+++ src/components/BaseTable.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="base-table">
+    <div class="table-btn">
+      <el-button
+        v-for="(j, k) in props.tableBtn"
+        :key="k"
+        :size="j.size"
+        :type="j.type"
+        @click="handleEdit(j.method)"
+        >{{ j.name }}</el-button
+      >
+    </div>
+
+    <el-table :data="props.tableClum" style="width: 100%">
+      <template v-if="props.checkbox">
+        <el-table-column type="selection" width="55" />
+      </template>
+      <template v-for="(v, i) in props.tableClum" :key="i">
+        <template v-if="!v.type">
+          <el-table-column v-bind="v" />
+        </template>
+
+        <template v-if="v.type === 'btn'">
+          <el-table-column v-bind="v">
+            <template #default="scope">
+              <el-button
+                v-for="(item, index) in v.btns"
+                :key="index"
+                :size="item.size"
+                :type="item.type"
+                @click="handleEdit(item.method, scope.$index, scope.row)"
+                >{{ item.name }}</el-button
+              ></template
+            >
+          </el-table-column>
+        </template>
+      </template>
+    </el-table>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+const props = defineProps({
+  checkbox: {
+    type: Boolean,
+    default: false
+  },
+  tableClum: {
+    type: Array,
+    default: () => []
+  },
+  tableBtn: {
+    type: Array,
+    default: () => []
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.base-table {
+  background: #fff;
+  margin-bottom: 20px;
+  border-radius: 5px;
+  .table-btn {
+    border-radius: 5px 5px 0 0;
+    background: #fff;
+    padding: 20px;
+    border-bottom: 1px solid #ece8e8;
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -29,7 +29,8 @@ export const routes = [
         path: '/404',
         name: '404',
         component: () => import('../views/404')
-      }
+      },
+      { path: '*', redirect: '/404', hidden: true }
     ]
   },
   user,
