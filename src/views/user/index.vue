2df7582ebfdade53a68c4a54ed1c8b52d7467e75
supperT <97022976@QQ.com>
2022-07-08 20:21:41 +0800

    feat(components): 封装queryfrom公共组件

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,10 +1,71 @@
 <template>
-    <div class=''>用户管理</div>
+  <query-form :queryClum="queryClum" :model="query"></query-form>
+  <div class="">用户管理</div>
 </template>
 
 <script setup>
+import QueryForm from '../../components/QueryForm.vue'
+import { reactive } from 'vue'
+
+const query = reactive({
+  userId: '',
+  userName: '',
+  state: ''
+})
+const queryClum = reactive([
+  {
+    label: '用户ID',
+    prop: 'userId',
+    type: 'text',
+    placeholder: '请输入用户ID'
+  },
+  {
+    label: '用户名称',
+    prop: 'userName',
+    type: 'text',
+    placeholder: '请输入用户名称'
+  },
+  {
+    label: '状态',
+    prop: 'state',
+    type: 'option',
+    placeholder: '',
+    options: [
+      {
+        label: '所有',
+        value: 0
+      },
+      {
+        label: '在职',
+        value: 1
+      },
+      {
+        label: '离职',
+        value: 2
+      },
+      {
+        label: '试用期',
+        value: 3
+      }
+    ]
+  },
+  {
+    type: 'btn',
+    btns: [
+      {
+        name: '查询',
+        type: 'primary',
+        method: 'query',
+        size: 'default'
+      },
+      {
+        name: '重置',
+        method: 'reset',
+        size: 'default'
+      }
+    ]
+  }
+])
 </script>
 
-<style lang='scss' scoped>
-
-</style>
+<style lang="scss" scoped></style>
